e4571b6eb8d82e189083796d1a1fa72417b54530
Hoyoung Yi <eric250@naver.com>
2025-02-28 23:59:09 +0900

    9498번

---
+++ 9498.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const rawInput = fs.readFileSync("input").toString().trim(); // "/dev/stdin"
+const input = rawInput;
+
+const score = input;
+
+if (score >= 90) console.log("A");
+else if (score >= 80) console.log("B");
+else if (score >= 70) console.log("C");
+else if (score >= 60) console.log("D");
+else console.log("F");
