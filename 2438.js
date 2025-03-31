68213f562c6d7b732ce5dac5a6c49a3c28ee8dcb
Hoyoung Yi <eric250@naver.com>
2025-03-31 11:30:01 +0900

    2438번

---
+++ 2438.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const rawInput = fs.readFileSync("input").toString().trim(); // fs.readFileSync(0, "utf-8").trim()
+const input = rawInput;
+
+const N = Number(input);
+
+const output = Array(N)
+  .fill()
+  .map((_, i) => Array(i + 1).fill("*").join(""))
+  .join("\n");
+
+console.log(output);
