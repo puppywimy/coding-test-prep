3f648e6c2ca9484b949a8d0d57a2869b6caecf7b
Hoyoung Yi <eric250@naver.com>
2025-03-31 11:28:57 +0900

    10950번

---
+++ 10950.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const rawInput = fs.readFileSync("input").toString().trim(); // fs.readFileSync(0, "utf-8").trim()
+const input = rawInput.split("\n").map((row) => row.split(" ").map(Number));
+
+const testCases = input.slice(1, input.length);
+
+testCases.forEach(([a, b]) => {
+  console.log(a + b);
+});
