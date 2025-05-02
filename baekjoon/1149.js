db76cc8726d0369418db5a213c02b8c04a58be84
Hoyoung Yi <eric250@naver.com>
2025-05-02 17:03:26 +0900

    feature(baekjoon): 1149번

---
+++ baekjoon/1149.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const rawInput = fs.readFileSync("input").toString().trim(); // fs.readFileSync(0, "utf-8").trim()
+
+const houses = rawInput
+  .split("\n")
+  .slice(1)
+  .map((row) => row.split(" ").map(Number));
+const table = [null, houses[0]];
+for (let i = 2; i <= houses.length; i++) {
+  table[i] = [
+    Math.min(table[i - 1][1], table[i - 1][2]) + houses[i - 1][0],
+    Math.min(table[i - 1][0], table[i - 1][2]) + houses[i - 1][1],
+    Math.min(table[i - 1][0], table[i - 1][1]) + houses[i - 1][2],
+  ];
+}
+
+console.log(Math.min(...table[table.length - 1]));
