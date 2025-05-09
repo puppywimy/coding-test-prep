0084eb2d061a6a120647cdd7f15adfc431726053
Hoyoung Yi <eric250@naver.com>
2025-05-09 12:04:33 +0900

    refactor(programmers/전화번호 목록.js): 풀이 방법 수정

--- programmers/전화번호 목록.js
+++ programmers/전화번호 목록.js
@@ -1,11 +1,4 @@
 function solution(phone_book) {
-  const phones = phone_book.slice().sort((a, b) => a.length - b.length);
-  const set = new Set();
-  for (const phone of phones) {
-    for (let i = 0; i < phone.length; i++) {
-      if (set.has(phone.slice(0, i + 1))) return false;
-    }
-    set.add(phone);
-  }
-  return true;
+  const phones = [...phone_book].sort();
+  return !phones.some((_, i, origin) => origin[i + 1]?.startsWith(origin[i]));
 }
