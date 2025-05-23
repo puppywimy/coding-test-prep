232efd6a7f696107258e369a67f38de0f4afaee2
Hoyoung Yi <eric250@naver.com>
2025-05-23 18:26:37 +0900

    refactor(programmers/H-Index.py): 불필요한 조건 제거(문제를 잘못 이해함.)

--- programmers/H-Index.py
+++ programmers/H-Index.py
@@ -2,7 +2,7 @@ def solution(citations):
     citations.sort()
     h = 0
     cursor = 0
-    while cursor <= h and h <= len(citations) - cursor:
+    while h <= len(citations) - cursor:
         if h > citations[cursor]:
             cursor += 1
         else:
