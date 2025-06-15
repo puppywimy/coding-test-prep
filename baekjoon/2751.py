e6872ed9b131c5b561a83f0762783f9fb79495bc
Hoyoung Yi <eric250@naver.com>
2025-06-15 12:42:43 +0900

    refactor(baekjoon/2751.py): mergeSort 함수 분리

--- baekjoon/2751.py
+++ baekjoon/2751.py
@@ -7,41 +7,45 @@ sys.stdin = open("input", "r")
 
 N = int(input())
 array = [int(input()) for _ in range(N)]
-tempArray = [None for _ in range(N)]
 
 
-# endIndex는 포함하지 않음.
-def merge(startIndex, endIndex):
-    middleIndex = math.floor((startIndex + endIndex) / 2)
-    cursor1 = startIndex
-    cursor2 = middleIndex
-    for i in range(startIndex, endIndex):
-        if cursor1 == middleIndex:
-            tempArray[i] = array[cursor2]
-            cursor2 += 1
-        elif cursor2 == endIndex:
-            tempArray[i] = array[cursor1]
-            cursor1 += 1
-        elif array[cursor1] <= array[cursor2]:
-            tempArray[i] = array[cursor1]
-            cursor1 += 1
-        else:
-            tempArray[i] = array[cursor2]
-            cursor2 += 1
-    for i in range(startIndex, endIndex):
-        array[i] = tempArray[i]
+def mergeSort(array):
+    _array = array[:]
+    tempArray = [None] * len(_array)
+
+    # endIndex는 포함하지 않음.
+    def _merge(startIndex, endIndex):
+        middleIndex = math.floor((startIndex + endIndex) / 2)
+        cursor1 = startIndex
+        cursor2 = middleIndex
+        for i in range(startIndex, endIndex):
+            if cursor1 == middleIndex:
+                tempArray[i] = _array[cursor2]
+                cursor2 += 1
+            elif cursor2 == endIndex:
+                tempArray[i] = _array[cursor1]
+                cursor1 += 1
+            elif _array[cursor1] <= _array[cursor2]:
+                tempArray[i] = _array[cursor1]
+                cursor1 += 1
+            else:
+                tempArray[i] = _array[cursor2]
+                cursor2 += 1
+        for i in range(startIndex, endIndex):
+            _array[i] = tempArray[i]
+
+    # endIndex는 포함하지 않음.
+    def _mergeSort(startIndex, endIndex):
+        if endIndex - startIndex == 1:
+            return
+        middleIndex = math.floor((startIndex + endIndex) / 2)
+        _mergeSort(startIndex, middleIndex)
+        _mergeSort(middleIndex, endIndex)
+        _merge(startIndex, endIndex)
+
+    _mergeSort(0, len(_array))
+
+    return _array
 
 
-# endIndex는 포함하지 않음.
-def mergeSort(startIndex, endIndex):
-    if endIndex - startIndex == 1:
-        return
-    middleIndex = math.floor((startIndex + endIndex) / 2)
-    mergeSort(startIndex, middleIndex)
-    mergeSort(middleIndex, endIndex)
-    merge(startIndex, endIndex)
-
-
-mergeSort(0, N)
-
-print("\n".join(map(str, array)))
+print("\n".join(map(str, mergeSort(array))))
