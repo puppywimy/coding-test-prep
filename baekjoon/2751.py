2b53395dd7d1a3c11ccac15c1d762df12ca96616
Hoyoung Yi <eric250@naver.com>
2025-06-15 13:36:51 +0900

    feature(baekjoon/2751.py): add quickSort() (feat. GPT - pivot을 랜덤하게 선택하는 방법)

--- baekjoon/2751.py
+++ baekjoon/2751.py
@@ -1,6 +1,7 @@
 # 제출 시 파일 입출력 부분 지우기
 
 import math
+import random
 import sys
 
 sys.stdin = open("input", "r")
@@ -48,4 +49,34 @@ def mergeSort(array):
     return _array
 
 
-print("\n".join(map(str, mergeSort(array))))
+def quickSort(array):
+    _array = array[:]
+
+    # endIndex는 포함하지 않음.
+    def _quickSort(startIndex, endIndex):
+        if startIndex == len(_array) or endIndex - startIndex <= 1:
+            return
+        pivotIndex = random.randrange(startIndex, endIndex)
+        _array[pivotIndex], _array[startIndex] = _array[startIndex], _array[pivotIndex]
+        pivotIndex = startIndex
+        cursor1 = startIndex + 1
+        cursor2 = endIndex - 1
+        while cursor1 - cursor2 < 1:
+            if (
+                _array[cursor1] > _array[pivotIndex]
+                and _array[cursor2] < _array[pivotIndex]
+            ):
+                _array[cursor1], _array[cursor2] = _array[cursor2], _array[cursor1]
+            if _array[cursor1] <= _array[pivotIndex]:
+                cursor1 += 1
+            if _array[cursor2] >= _array[pivotIndex]:
+                cursor2 -= 1
+        _array[pivotIndex], _array[cursor2] = _array[cursor2], _array[pivotIndex]
+        _quickSort(startIndex, cursor2)
+        _quickSort(cursor2 + 1, endIndex)
+
+    _quickSort(0, len(_array))
+    return _array
+
+
+print("\n".join(map(str, quickSort(array))))
