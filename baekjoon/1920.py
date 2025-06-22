973e3a0a663133882b7c7c035921605d4c4f5f90
Hoyoung Yi <eric250@naver.com>
2025-06-22 16:53:08 +0900

    feature(baekjoon/1920.py)

---
+++ baekjoon/1920.py
@@ -0,0 +1,36 @@
+# 제출 시 파일 입출력 부분 지우기
+
+import sys
+
+sys.stdin = open("input", "r")
+
+N = int(input())
+numbers = list(map(int, input().split(" ")))
+M = int(input())
+targetNumberStrings = input().split(" ")
+targetNumberDatas = [
+    {"i": i, "number": int(targetNumberStrings[i]), "foundInNumbers": False}
+    for i in range(len(targetNumberStrings))
+]
+numbers.sort()
+targetNumberDatas.sort(key=lambda x: x["number"])
+cursor = 0
+for targetNumberData in targetNumberDatas:
+    while cursor < len(numbers):
+        if numbers[cursor] == targetNumberData["number"]:
+            targetNumberData["foundInNumbers"] = True
+            break
+        elif numbers[cursor] > targetNumberData["number"]:
+            break
+        else:
+            cursor += 1
+targetNumberDatas.sort(key=lambda x: x["i"])
+
+print(
+    "\n".join(
+        map(
+            lambda targetNumberData: "1" if targetNumberData["foundInNumbers"] else "0",
+            targetNumberDatas,
+        )
+    )
+)
