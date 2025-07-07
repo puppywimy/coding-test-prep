023c91a281ffabe910a67bbbcc0b0659630d2a65
Hoyoung Yi <eric250@naver.com>
2025-07-07 22:43:01 +0900

    feature(programmers/lv1_이상한 문자 만들기.py)

---
+++ programmers/lv1_이상한 문자 만들기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    output = []
+    for word in s.split(' '):
+        characters = list(word)
+        for i in range(len(characters)):
+            if i % 2 == 0:
+                characters[i] = characters[i].upper()
+            else:
+                characters[i] = characters[i].lower()
+        output.append(''.join(characters))
+    return ' '.join(output)
