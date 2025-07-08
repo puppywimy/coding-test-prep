493a77b7b4498ec0b52de219f3d6ad6bd6fcafdb
Hoyoung Yi <eric250@naver.com>
2025-07-08 22:23:37 +0900

    style(programmers/lv1_이상한 문자 만들기.py): black formatter 적용

--- programmers/lv1_이상한 문자 만들기.py
+++ programmers/lv1_이상한 문자 만들기.py
@@ -1,11 +1,11 @@
 def solution(s):
     output = []
-    for word in s.split(' '):
+    for word in s.split(" "):
         characters = list(word)
         for i in range(len(characters)):
             if i % 2 == 0:
                 characters[i] = characters[i].upper()
             else:
                 characters[i] = characters[i].lower()
-        output.append(''.join(characters))
-    return ' '.join(output)
+        output.append("".join(characters))
+    return " ".join(output)
