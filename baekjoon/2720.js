8f1267f632f12cdc0ba351e223fb907ef19a6eec
Hoyoung Yi <eric250@naver.com>
2025-09-22 11:07:00 +0900

    feature(baekjoon/2720.js)

---
+++ baekjoon/2720.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const rawInput = fs.readFileSync("input").toString().trim(); // fs.readFileSync(0, "utf-8").trim();
+
+function solution() {
+  const [T, ...changes] = rawInput.split("\n").map(Number);
+  for (let i = 0; i < T; i++) {
+    let change = changes[i];
+    const coins = [];
+    while (change > 0) {
+      const quarter = Math.floor(change / 25);
+      coins.push(quarter);
+      if (quarter > 0) change %= 25;
+      const dime = Math.floor(change / 10);
+      coins.push(dime);
+      if (dime > 0) change %= 10;
+      const nickel = Math.floor(change / 5);
+      coins.push(nickel);
+      if (nickel > 0) change %= 5;
+      const penny = Math.floor(change / 1);
+      coins.push(penny);
+      if (penny > 0) change %= 1;
+    }
+    console.log(coins.join(" "));
+  }
+}
+
+solution();
