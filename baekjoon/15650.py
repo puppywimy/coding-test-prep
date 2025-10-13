90cb60e362b0512a667303ca486cdb94f035a80b
Hoyoung Yi <eric250@naver.com>
2025-10-13 20:35:22 +0900

    solve(baekjoon/15650.py)

---
+++ baekjoon/15650.py
@@ -0,0 +1,29 @@
+# 제출 시 파일 입출력 부분 지우기
+
+import sys
+
+sys.stdin = open("input", "r")
+
+
+def solution():
+    N, M = map(int, input().split(" "))
+    isUsed = [False for _ in range(N + 1)]  # 1-based indexing
+
+    def recursive(count, lastNumber):
+        if count == M:
+            selections = []
+            for i in range(N + 1):
+                if isUsed[i]:
+                    selections.append(i)
+            print(" ".join(map(str, selections)))
+            return
+
+        for number in range(lastNumber + 1, N + 1):
+            isUsed[number] = True
+            recursive(count + 1, number)
+            isUsed[number] = False
+
+    recursive(0, 0)
+
+
+solution()
