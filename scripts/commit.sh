6e3fddede7bc63f8c47da8c16b2c000ec5cf757d
Hoyoung Yi <eric250@naver.com>
2025-11-21 14:53:56 +0900

    feature(scripts): prompt 기능 추가

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,12 +1,19 @@
-git add .
+#! /usr/bin/env zsh
 
-SCOPE=$(git diff --name-only --cached)
-COMMIT_MESSAGE="solve($SCOPE)"
-
-if [ -z $1 ]; then
-  git commit -m "$COMMIT_MESSAGE"
-else
-  git commit -m "$COMMIT_MESSAGE: $1"
+untrackedFiles=$(git ls-files -o --exclude-standard --directory)
+echo "Untracked Files는 다음과 같습니다.\n$untrackedFiles\n"
+read "answer?모든 변경 사항을 커밋하시겠습니까? [Y/n] "
+if [[ $answer != "Y" ]]; then
+  exit
 fi
 
-echo "\n성공적으로 커밋했습니다."
+git add .
+scope=$(git diff --name-only --cached)
+commitMessage="solve($scope)"
+if [ -z $1 ]; then
+  git commit -m "$commitMessage"
+else
+  git commit -m "$commitMessage: $1"
+fi
+echo "\n"
+echo "성공적으로 커밋했습니다."
