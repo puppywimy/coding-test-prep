96cef830b777e1c93771c115df583339545bb110
puppywimy <eric250@naver.com>
2026-04-22 16:17:16 +0900

    solve(algorithms/programmers/lv0/120831.java)

---
+++ algorithms/programmers/lv0/120831.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        return IntStream.rangeClosed(2, n).filter(element -> element % 2 == 0).sum();
+    }
+}
