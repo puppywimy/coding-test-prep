bad87e8f75f16ba4187d71069457b32967f51dff
puppywimy <eric250@naver.com>
2026-04-22 18:22:59 +0900

    solve(algorithms/programmers/lv0/120837.java)

---
+++ algorithms/programmers/lv0/120837.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int hp) {
+        int count = 0;
+        count += hp / 5;
+        hp %= 5;
+        count += hp / 3;
+        hp %= 3;
+        count += hp;
+        return count;
+    }
+}
